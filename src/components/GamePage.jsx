e782fe99f70b069e25ce004de818ed333e2bd653
Fraser <Fraser.Hughes@btinternet.com>
2021-09-30 20:58:03 +0200

    More css changes and small rework on game logic

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -3,27 +3,36 @@ import { Container, Image, Card } from "semantic-ui-react";
 
 const Game = () => {
   const [playerOption, setPlayerOption] = useState();
-  let options = ["rock", "paper", "scissors"];
-  let compOption = options[Math.floor(Math.random() * options.length)];
+  const [compOption, setCompOption] = useState();
+
+  let options = ["Rock", "Paper", "Scissors"];
+
+  const playGame = (Option) => {
+    randomChoice()
+    playerDecision(Option)
+  }
+
+  const randomChoice = () => {
+    setCompOption(options[Math.floor(Math.random() * options.length)]);
+  };
 
   const playerDecision = (Option) => {
     setPlayerOption(Option);
-    return gameWinner(playerOption);
   };
 
   const gameWinner = () => {
     let winner;
-    if (compOption === "rock" && playerOption === "scissors") {
+    if (compOption === "Rock" && playerOption === "Scissors") {
       winner = "Computer Wins!";
-    } else if (compOption === "paper" && playerOption === "rock") {
+    } else if (compOption === "Paper" && playerOption === "Rock") {
       winner = "Computer Wins!";
-    } else if (compOption === "scissors" && playerOption === "paper") {
+    } else if (compOption === "Scissors" && playerOption === "Paper") {
       winner = "Computer Wins!";
-    } else if (compOption === "rock" && playerOption === "paper") {
+    } else if (compOption === "Rock" && playerOption === "Paper") {
       winner = "Player Wins!";
-    } else if (compOption === "paper" && playerOption === "scissors") {
+    } else if (compOption === "Paper" && playerOption === "Scissors") {
       winner = "Player Wins!";
-    } else if (compOption === "scissors" && playerOption === "rock") {
+    } else if (compOption === "Scissors" && playerOption === "Rock") {
       winner = "Player Wins!";
     } else if (compOption === playerOption) {
       winner = "It's a draw!";
@@ -33,11 +42,13 @@ const Game = () => {
 
   return (
     <Container textAlign="center">
-      <h1 data-cy="game-header">Game On!</h1>
-      <button data-cy="rock-button" onClick={() => playerDecision("rock")}>
+      <h1 class="headers" data-cy="game-header">
+        Game On!
+      </h1>
+      <button data-cy="rock-button" onClick={() => playGame("Rock")}>
         <Image
           src="https://image.shutterstock.com/image-vector/pile-boulders-rocks-260nw-60307183.jpg"
-          size="small"
+          size="medium"
         ></Image>
       </button>
       <Card centered raised>
@@ -58,19 +69,19 @@ const Game = () => {
           <br />
         </ul>
       </Card>
-      <button data-cy="paper-button" onClick={() => playerDecision("paper")}>
+      <button data-cy="paper-button" onClick={() => playGame("Paper")}>
         <Image
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREALCV4PXDrTnrpHs-pQEkNG5HYqMod6i7zg&usqp=CAU"
-          size="small"
+          size="medium"
         ></Image>
       </button>
       <button
         data-cy="scissors-button"
-        onClick={() => playerDecision("scissors")}
+        onClick={() => playGame("Scissors")}
       >
         <Image
           src="https://img1.pnghut.com/3/13/3/DxggERCLCj/scissors-haircutting-shears-cutting.jpg"
-          size="small"
+          size="medium"
         ></Image>
       </button>
     </Container>
--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { Container, Button, Header } from "semantic-ui-react";
-import { Link } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
 const Welcome = () => {
   return (
-    <Container color="red" textAlign="center" padded size="massive" raised>
-      <Header data-cy="welcome-header">Lets Play ROCK, PAPER, SCISSORS</Header>
-      <Button data-cy="play-button" as={Link} to={{ pathname: "/gamepage" }}>
-        Play Game!
-      </Button>
+    <div top="20px">
+    <Container textAlign="center">
+      <h1 className="headers" data-cy="welcome-header">
+        Lets Play ROCK, PAPER, SCISSORS
+      </h1>
+      <a href="/gamepage" className="btn" data-cy="play-button">
+        Start Game!
+      </a>
     </Container>
+    </div>
   );
 };
 
