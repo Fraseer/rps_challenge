f8c8546d6d01cb648e660466cf5d9581831046ba
Fraser <Fraser.Hughes@btinternet.com>
2021-09-30 22:03:19 +0200

    Adds redirects file

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-// import { Link } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import { Container} from "semantic-ui-react";
 
 const Welcome = () => {
   return (
@@ -9,17 +8,9 @@ const Welcome = () => {
         <h1 className="headers" data-cy="welcome-header">
           Lets Play ROCK, PAPER, SCISSORS
         </h1>
-        <a href="https://srossicsrepapkcor.netlify.app/gamepage" className="btn" data-cy="play-button">
+        <a href="/gamepage" className="btn" data-cy="play-button">
           Start Game!
         </a>
-
-         {/* <a as={Link} to={{ pathname: "/gamepage" }} className="btn" data-cy="play-button">
-          Start Game!
-        </a> */}
-
-        {/* <Button data-cy="play-button" as={Link} to={{ pathname: "/gamepage" }}>
-          Start Game!
-        </Button> */}
       </Container>
     </div>
   );
