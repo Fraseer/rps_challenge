12e693ae519532c8e8466dab5505a2f61ac2f2d6
Fraser <Fraser.Hughes@btinternet.com>
2021-09-30 21:29:02 +0200

    Revert to previous button

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container } from "semantic-ui-react";
-
+import { Container, Button } from "semantic-ui-react";
 
 const Welcome = () => {
   return (
     <div top="20px">
-    <Container textAlign="center">
-      <h1 className="headers" data-cy="welcome-header">
-        Lets Play ROCK, PAPER, SCISSORS
-      </h1>
-      <a href="/gamepage" as={Link} to={{pathname: "/gamepage"}} className="btn" data-cy="play-button">
-        Start Game!
-      </a>
-    </Container>
+      <Container textAlign="center">
+        <h1 className="headers" data-cy="welcome-header">
+          Lets Play ROCK, PAPER, SCISSORS
+        </h1>
+        <Button data-cy="play-button" as={Link} to={{ pathname: "/gamepage" }}>
+          Start Game!
+        </Button>
+      </Container>
     </div>
   );
 };
